02a45aeca1d4cfc46c244fa83b7222f97d154ed4
Satoshi <140612116+satohshi@users.noreply.github.com>
2024-12-12 23:12:15 +0900

    add overloads and remove @ts-ignore

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,6 @@ import type { BodyParams } from './body-params.js'
 
 export type { UniqueCollection } from './type-utils.js'
 
-// ---------------------------------------------------------------------------------------------------------
-// `@ts-ignore`s are used to suppress the error caused by the type mismatch between
-// the type signature of the method in the base class and the overridden method in the subclass.
-// ---------------------------------------------------------------------------------------------------------
-
 export class PocketBaseTS<
 	TSchema extends SchemaDeclaration,
 	TMaxDepth extends 0 | 1 | 2 | 3 | 4 | 5 | 6 = 2,
@@ -30,7 +25,6 @@ export class PocketBaseTS<
 		super(baseUrl, authStore, lang)
 	}
 
-	// @ts-ignore
 	override collection<TName extends (keyof TSchema & string) | (string & {})>(
 		idOrName: TName
 	): RecordServiceTS<TSchema, TName, TMaxDepth> {
@@ -49,6 +43,7 @@ class RecordServiceTS<
 	TSchema extends SchemaDeclaration,
 	TKey extends keyof TSchema,
 	TMaxDepth extends number,
+	_Type = TSchema[TKey]['type'],
 	_ListOptions extends Options<TSchema, TKey, TMaxDepth, true> = Options<
 		TSchema,
 		TKey,
@@ -84,53 +79,82 @@ class RecordServiceTS<
 		return super.getFullList(processedOption)
 	}
 
-	// @ts-ignore
+	override async getList(page: number, perPage: number): Promise<ListResult<_Type>>
+	override async getList<const TOptions extends _ListOptions>(
+		page: number,
+		perPage: number,
+		options: TOptions
+	): Promise<ListResult<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>>
 	override async getList<const TOptions extends _ListOptions>(
 		page = 1,
 		perPage = 30,
 		options?: TOptions
-	): Promise<ListResult<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>> {
+	): Promise<ListResult<_Type> | ListResult<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>> {
 		const processedOption = processOptions(options)
 		return super.getList(page, perPage, processedOption)
 	}
 
-	// @ts-ignore
+	override async getFirstListItem(
+		filter: string | FilterHelper<TSchema, TKey, TMaxDepth, true>
+	): Promise<_Type>
+	override async getFirstListItem<const TOptions extends _ListOptions>(
+		filter: string | FilterHelper<TSchema, TKey, TMaxDepth, true>,
+		options: TOptions
+	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>
 	override async getFirstListItem<const TOptions extends _ListOptions>(
 		filter: string | FilterHelper<TSchema, TKey, TMaxDepth, true>,
 		options?: TOptions
-	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
+	): Promise<_Type | PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
 		const processedFilter = processFilterAndSort(filter)
 		const processedOption = processOptions(options)
 		return super.getFirstListItem(processedFilter, processedOption)
 	}
 
-	// @ts-ignore
+	override async getOne(id: string): Promise<_Type>
+	override async getOne<const TOptions extends _ViewOptions>(
+		id: string,
+		options: TOptions
+	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>
 	override async getOne<const TOptions extends _ViewOptions>(
 		id: string,
 		options?: TOptions
-	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
+	): Promise<_Type | PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
 		const processedOption = processOptions(options)
 		return super.getOne(id, processedOption)
 	}
 
-	// @ts-ignore
+	override async create(
+		bodyParams?: Partial<Omit<_Type, 'created' | 'updated'>> | { [key: string]: any } | FormData
+	): Promise<_Type>
+	override async create<const TOptions extends _ViewOptions>(
+		bodyParams: Partial<Omit<_Type, 'created' | 'updated'>> | { [key: string]: any } | FormData,
+		options: TOptions
+	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>
 	override async create<const TOptions extends _ViewOptions>(
 		bodyParams?:
-			| Partial<Omit<TSchema[TKey]['type'], 'created' | 'updated'>>
+			| Partial<Omit<_Type, 'created' | 'updated'>>
 			| { [key: string]: any }
 			| FormData,
 		options?: TOptions
-	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
+	): Promise<_Type | PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
 		const processedOption = processOptions(options)
 		return super.create(bodyParams, processedOption)
 	}
 
-	// @ts-ignore
+	override async update(
+		id: string,
+		bodyParams?: BodyParams<_Type> | { [key: string]: any } | FormData
+	): Promise<_Type>
 	override async update<const TOptions extends _ViewOptions>(
 		id: string,
-		bodyParams?: BodyParams<TSchema[TKey]['type']> | { [key: string]: any } | FormData,
+		bodyParams: BodyParams<_Type> | { [key: string]: any } | FormData,
+		options: TOptions
+	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>>
+	override async update<const TOptions extends _ViewOptions>(
+		id: string,
+		bodyParams?: BodyParams<_Type> | { [key: string]: any } | FormData,
 		options?: TOptions
-	): Promise<PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
+	): Promise<_Type | PBResponseType<TSchema, TKey, TOptions, TMaxDepth>> {
 		const processedOption = processOptions(options)
 		return super.update(id, bodyParams, processedOption)
 	}
